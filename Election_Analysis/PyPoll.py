c8c47580a119ec4fe121a8bbb96178c6e725d56e
ambermiddendorf <95837693+ambermiddendorf@users.noreply.github.com>
2022-01-11 20:15:26 -0600

    Complete results with county and candidate winners

--- Election_Analysis/PyPoll.py
+++ Election_Analysis/PyPoll.py
@@ -13,6 +13,13 @@ candidate_votes={}
 winning_candidate=""
 winning_count = 0
 winning_per = 0
+#County Counts trackers.
+county_options=[]
+county_votes={}
+#Winning county tracker.
+c_winning_count=0
+c_winning_per=0
+c_winning_county=""
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -20,14 +27,21 @@ with open(file_to_load) as election_data:
 #read the header row.
     headers = next(file_reader)
 
-#Count votes, collect candidate names, add unique names to candidate_options list.
+#Count votes, 
     for row in file_reader:
         total_votes +=1
         candidate_name=row[2]
+        county_name=row[1]
+#collect candidate names, add unique names to candidate_options list, count votes for each candidate.
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name]=0
         candidate_votes[candidate_name] +=1
+#collect county names, add unique names to county_options and count votes for each county.
+        if county_name not in county_options:
+            county_options.append(county_name)
+            county_votes[county_name]=0
+        county_votes[county_name] +=1
 
 #Save results to file.
 with open(file_to_save, "w") as txt_file:
@@ -36,14 +50,38 @@ with open(file_to_save, "w") as txt_file:
         f'\nElection Results\n'
         f'--------------------------\n'
         f'Total Votes: {total_votes:,}\n'
-        f'---------------------------\n')
+        f'---------------------------\n'
+        f'County Votes: \n')
     print(election_results, end="")
-    #Save teh final vote count to the text file.
+    #Save the final vote count to the text file.
     txt_file.write(election_results)
-    #Print total votes.
-    #print(total_votes)
-    #print(candidate_options)
-    #print(candidate_votes)
+
+    for county_name in county_votes:
+#retrieve votes counts per county.
+        c_votes = county_votes[county_name]
+#calcluate vote percentage per county.
+        c_vote_per = float(c_votes)/float(total_votes) * 100
+#print results for each county.
+        county_results = (
+        f'{county_name}: {c_vote_per:.1f}% ({c_votes:,})\n')
+# Print each county, their voter count, and percentage to the terminal.
+        print(county_results)
+#  Save the county results to our text file.
+        txt_file.write(county_results)
+#Determine the winning County        
+        if (c_votes > c_winning_count) and (c_vote_per > c_winning_per):
+            c_winning_count = c_votes
+            c_winning_per = c_vote_per
+            c_winning_county = county_name
+    c_winningSummary= (
+        f'------------------------------\n'
+        f'Largest County Turnout: {c_winning_county} \n'
+        f'-------------------------------\n')
+
+    print(c_winningSummary)
+#Save the winning county's results to the text file.
+    txt_file.write(c_winningSummary)
+
     #Iterate thru candidate list.
     for candidate_name in candidate_votes:
     #retrieve votes counts per candidate.
@@ -56,17 +94,18 @@ with open(file_to_save, "w") as txt_file:
         print(candidate_results)
 #  Save the candidate results to our text file.
         txt_file.write(candidate_results)
-    #Determine winning vote count and candidate.
         #Determine if the votes are greater than the winning count.
         if (votes > winning_count) and (vote_per > winning_per):
             winning_count = votes
             winning_per = vote_per
             winning_candidate = candidate_name
     winningSummary= (
-            f'------------------------------\n'
-            f'Winner: {winning_candidate} \n'
-            f'Winning Vote Count: {winning_count:,}\n'
-            f'Winning percentage: {winning_per:.1f}% \n')
+        f'------------------------------\n'
+        f'Winner: {winning_candidate} \n'
+        f'Winning Vote Count: {winning_count:,}\n'
+        f'Winning percentage: {winning_per:.1f}% \n')
+
     print(winningSummary)
-    #Save the winning candidate's results to the text file.
+#Save the winning candidate's results to the text file.
     txt_file.write(winningSummary)
+
--- Election_Analysis/README.md
+++ Election_Analysis/README.md
@@ -1,2 +1,28 @@
-# Election_Analsyis
-Module 3 with Python
+# Election_Analysis
+## Project Overview
+The Colorado Board of Elections has asked for an audit of a local congressional election. The asked for the following information:
+1. Calculate the total number of votes cast.
+2. List all the candidates that received votes.
+3. Report the total number of votes for each of the candidates.
+4. Report the percentage of the vote each candidate received.
+5. Report the overall winner of the election based on popular vote.
+
+## Resources
+* Data Source: election_results.csv
+* Software: Python 3.7.6, Visual Studio Code 1.63.2
+
+## Summary
+From the data provided, the following was found:
+
+Total Votes: 369,711
+
+Votes were received by these 3 candidates:
+1. Charles Casper Stockham: 23.0% (85,213)
+2. Diana DeGette: 73.8% (272,892)
+3. Raymon Anthony Doane: 3.1% (11,606)
+
+The Winner: Diana DeGette with 272,892 votes equaling 73.8% of the vote. 
+
+## Challenge Overview
+
+## Challenge Summary
